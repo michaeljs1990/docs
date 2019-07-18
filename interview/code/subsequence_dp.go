fc626348d084957a744886fd333a506a3ca4e525
Michael Schuett <michaeljs1990@gmail.com>
2019-07-18 00:27:11 -0400

    fmt code

--- interview/code/subsequence_dp.go
+++ interview/code/subsequence_dp.go
@@ -18,10 +18,10 @@ import (
 // Yes... it's true golang doesn't have a simple
 // max function for ints...
 func Max(x, y int) int {
-    if x < y {
-        return y
-    }
-    return x
+	if x < y {
+		return y
+	}
+	return x
 }
 
 // Example of how the solution builds up it's answer for the inputs
@@ -41,35 +41,35 @@ func Max(x, y int) int {
 // non matches we take the larger of item above it or to the left of it.
 func solver(a, b string) int {
 
-  // Create table to hold all calls the +1 is needed for holding
-  // the empty string
-  memo := make([][]int, len(a)+1)
-  for i := range memo {
-    memo[i] = make([]int, len(b)+1)
-  }
+	// Create table to hold all calls the +1 is needed for holding
+	// the empty string
+	memo := make([][]int, len(a)+1)
+	for i := range memo {
+		memo[i] = make([]int, len(b)+1)
+	}
 
-  // Fill in the first column so everything is 0. This may seem a bit strange
-  // but it's setting up the case of having an empty string so the rest is easy
-  // to setup later. The next loop does the same but for the first row.
-  for i := 0; i < len(a); i++ {
-    memo[i][0] = 0
-  }
+	// Fill in the first column so everything is 0. This may seem a bit strange
+	// but it's setting up the case of having an empty string so the rest is easy
+	// to setup later. The next loop does the same but for the first row.
+	for i := 0; i < len(a); i++ {
+		memo[i][0] = 0
+	}
 
-  for i := 0; i < len(b); i++ {
-    memo[0][i] = 0
-  }
+	for i := 0; i < len(b); i++ {
+		memo[0][i] = 0
+	}
 
-  for i := 1; i <= len(a); i++ {
-    for j := 1; j <= len(b); j++ {
-      if a[i-1] == b[j-1] {
-        memo[i][j] = memo[i-1][j-1] + 1
-      } else {
-        memo[i][j] = Max(memo[i-1][j], memo[i][j-1])
-      }
-    }
-  }
+	for i := 1; i <= len(a); i++ {
+		for j := 1; j <= len(b); j++ {
+			if a[i-1] == b[j-1] {
+				memo[i][j] = memo[i-1][j-1] + 1
+			} else {
+				memo[i][j] = Max(memo[i-1][j], memo[i][j-1])
+			}
+		}
+	}
 
-  return memo[len(a)][len(b)]
+	return memo[len(a)][len(b)]
 }
 
 func main() {
